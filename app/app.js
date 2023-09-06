b7e6f331ac65382f2761d40a92537ded8f357b15
Igor Seliverstov <igrseliwjorstow@bk.ru>
2023-09-06 11:54:15 +0300

    подключил базу, донастроил проект

--- app/app.js
+++ app/app.js
@@ -5,6 +5,22 @@ const app = express()
 // Router
 const router = express.Router()
 
+//DB
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end();
+})
+
 // Главная
 router.get('/', (_req, res) => {
     res.status(200).json({
@@ -22,6 +38,6 @@ router.get('/*', (_req, res) => {
 // Routes
 app.use('/', router)
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Сервер запущен')
 })
